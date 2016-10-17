3b516d15b8dc34f8a4db426eccab42dd3e82a91b
Code <code@SEDC.local>
2016-10-17 21:15:07 +0200

    include files

---
+++ cas05/header.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            <?php
+            if(isset($title) && is_string($title)){
+            echo $title;
+            }
+            else{
+                echo "default";
+            }
+            
+        
+        ?></title>
+    </head>
+    
+
