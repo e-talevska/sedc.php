1e369ee1dd1b54281e67a0b9ac63ba881c500d51
e-talevska <e.talevska@hotmail.com>
2016-10-19 18:23:24 +0200

    forma

---
+++ cas06/example1/index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Forms</title>
+    </head>
+    <body>
+        <form method="post">
+            <p>
+                <label for="username">Username</label>
+                <input name="username" id="username" type="text">
+            </p>
+             <p>
+                <label for="pass">Password</label>
+                <input name="password" id="pass" type="password">
+            </p>
+            <p>
+                <!--<input type="submit" >-->
+                <button name="login" type="submit">Login</button>
+            </p>
+        </form>
+    </body>
+</html>
