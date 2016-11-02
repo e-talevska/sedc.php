8361e7f54952eb621d653fba8d1ea761569f3cd2
Darijan Shekerov <darijan2002@gmail.com>
2016-11-02 21:03:47 +0100

    bugfixes :P

--- cas9/fileupload/scandir.php
+++ cas9/fileupload/scandir.php
@@ -4,7 +4,7 @@ $dir_contents = scandir('uploads');
 var_dump($dir_contents);
 foreach ($dir_contents as $filename) {
     $pathinfo = pathinfo('uploads/'.$filename);
-    if(in_array($pathinfo['extension'], $extensions)) {
+    if(isset($pathinfo['extension']) && in_array($pathinfo['extension'], $extensions)) {
         ?>
 
         <img style="width: 100px; border: 1px solid black; margin: 1px; padding: 5px;"
