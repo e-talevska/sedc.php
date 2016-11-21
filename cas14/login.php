2ed98afde475c9e78ad0c278a924b82f3de30c28
Darijan Shekerov <darijan2002@gmail.com>
2016-11-21 21:17:04 +0100

    Cas14 - MySQL/PHP Login

---
+++ cas14/login.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * TODO:
+ * 1. Create a form
+ * 2. 'Submit' the form data
+ * 3. Check database for entered user's existence
+ * 4a. If user doesn't exist, print an exception
+ * 4b. Else, create a session with user's info
+ * 5. Redirect to a validation page (profile.php)
+ * */
