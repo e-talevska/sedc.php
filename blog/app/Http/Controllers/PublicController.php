4025709cd0c7be11fbad55726a882f6325fec37d
e-talevska <e.talevska@hotmail.com>
2016-11-28 18:03:13 +0100

    Laravel init

---
+++ blog/app/Http/Controllers/PublicController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function about() {
+        return "About us";
+    }
+}
