1400fce0a92d1ff370f3550ecd642031ec048dc9
zmejko <ace_zmej@hotmail.com>
2016-12-05 18:26:09 +0100

    laravel init

---
+++ blog/app/Http/Controllers/PublicController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+   public function about(){
+     //  $name = "Sedc";
+     //  $lastname = "<i>PHP</i>";
+       $values = [
+           'firstname' => 'Sedc',
+           'lastname' => 'PHP',
+           'myteam' => [
+               'John Doe',
+               'Jane Doe'
+  
+           ]
+       ];
+    return view ('public/about', $values);
+          //  ->with('firstname',$name)
+          //  ->with('lastname',$lastname);
+}
+}
+
